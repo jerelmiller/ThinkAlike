4df926949e11bf107005898c924ac45360dad388
Jerel Miller <jermille@mines.edu>
2013-06-29 14:51:43 -0600

    Initial commit for new ThinkAlike website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThinkAlike::Application.config.secret_token = '2dcd930dd2adc6ccb9c134b69ed081fb9b83a119c2a1b4622f2ae7c96e0b2c3aca6bfc4e1c50d7412de5d37ef32e323772dcc07d18a10eef9201b394efa4dda3'
