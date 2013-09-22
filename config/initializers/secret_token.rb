ac174592cb07de9c2f7c5f592b07e272c4d611d6
Jerel Miller <jerelmiller@gmail.com>
2013-09-22 13:50:40 -0600

    Upgrading to rails 4 and updating config files via rake rails:update

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ThinkAlike::Application.config.secret_token = '2dcd930dd2adc6ccb9c134b69ed081fb9b83a119c2a1b4622f2ae7c96e0b2c3aca6bfc4e1c50d7412de5d37ef32e323772dcc07d18a10eef9201b394efa4dda3'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThinkAlike::Application.config.secret_key_base = 'a077c6fd65cd54ca13d6543bc80dc0ae5230dee6f44890fa661ea7767521334520f2acdfc271e80aa17fc5bebdbaf84b05ab47366503cdf13595c5894decdbba'
